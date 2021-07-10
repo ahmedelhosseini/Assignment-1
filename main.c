22ad758841e1030892b2f0acd1e3bf82f9adb8ca
Ahmed Mohammed Hussanein <82288219+Ahmed-Mohammed-Hussanein@users.noreply.github.com>
2021-07-10 22:26:24 +0200

    add the dynamic array
    add the dynamic array feature

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <windows.h>
 
-nt currentNumber = 0;
+#define NumberOfStudent 500
+
+int currentNumber = 0;
 int Choice = 0;
 
 typedef struct
@@ -57,6 +59,128 @@ void pointHere(int x, int y)
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 }
 
+void Get_Info(Student* getStudent)
+{
+    system("cls");
+
+    setCursorPosition(0, 8);
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+
+    setCursorPosition(0, 16);
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+
+    fflush(stdin);
+    setCursorPosition(0, 9);
+    printf("Enter student name                          : ");
+    while(1 != scanf("%[a-zA-Z ?]+", getStudent->name))
+    {
+        setCursorPosition(0, 9);
+        printf("                                                           ");
+        setCursorPosition(0, 9);
+        printf("Please Enter a valid input name         : ");
+        fflush(stdin);
+    }
+
+    fflush(stdin);
+    setCursorPosition(0, 10);
+    printf("Enter student id                          : ");
+    while(1 != scanf("%d", &(getStudent->ID)))
+    {
+        setCursorPosition(0, 10);
+        printf("                                                           ");
+        setCursorPosition(0, 10);
+        printf("Please Enter a valid input id : ");
+        fflush(stdin);
+    }
+
+    fflush(stdin);
+    setCursorPosition(0, 11);
+    printf("Enter student birthday in format dd/mm/yyyy : ");
+    while(3 != scanf("%d/%d/%d",&(getStudent->day),&(getStudent->month),&(getStudent->year)))
+    {
+        setCursorPosition(0, 11);
+        printf("                                                           ");
+        setCursorPosition(0, 11);
+        printf("An Error happened\n");
+        getch();
+        setCursorPosition(0, 11);
+        printf("                                                           ");
+        setCursorPosition(0, 11);
+        printf("Please Enter student birthday in format dd/mm/yyyy : ");
+        fflush(stdin);
+    }
+
+    fflush(stdin);
+    setCursorPosition(0, 12);
+    printf("Enter student score                       : ");
+    while(1 != scanf("%d", &(getStudent->score)))
+    {
+        setCursorPosition(0, 12);
+        printf("                                                           ");
+        setCursorPosition(0, 12);
+        printf("Please Enter a valid input score : ");
+        fflush(stdin);
+    }
+}
+
+void Print(Student* printStudent, int row)
+{
+
+    setCursorPosition(0, row);   printf("%d",printStudent->ID);
+    setCursorPosition(15, row);  printf("%s",printStudent->name);
+    setCursorPosition(45, row);  printf("%d/%d/%d", printStudent->day, printStudent->month, printStudent->year);
+    setCursorPosition(60, row);  printf("%d",printStudent->score);
+}
+
+void PrintAll( )
+{
+    int row = 0;
+
+    system("cls");
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+    setCursorPosition(0, 0);      printf("ID");
+    setCursorPosition(15, 0);     printf("Name");
+    setCursorPosition(45, 0);     printf("BirthDay");
+    setCursorPosition(60, 0);     printf("Score");
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    if(1 != Choice)
+    {
+        struct node* temp = head;
+
+        while(NULL != temp)
+        {
+            Print(&(temp->student), ++row);
+            temp = temp->next;
+        }
+    }
+    else
+    {
+        for(; row < currentNumber; row++)
+        {
+            Print(&st[row], row + 1);
+        }
+    }
+
+    getch();
+}
+
+void Shift( Student* st1, Student* st2)
+{
+    strcpy(st2->name, st1->name);
+    st2->ID = st1->ID;
+    st2->day = st1->day;
+    st2->month = st1->month;
+    st2->year = st1->year;
+    st2->score = st1->score;
+}
+
 void InsertAtbeg(  )
 {
     if(1 != Choice)
@@ -72,7 +196,19 @@ void InsertAtbeg(  )
         head = add;
         
     }
-    
+    else
+    {
+        char i;
+        for(i=currentNumber; i>0;i--)
+        {
+            Shift((st+(i-1)), (st+i));
+        }
+
+        Get_Info(st);
+
+        currentNumber++;
+    }
+
     setCursorPosition(0, 13);
     printf("The student is inserted successfully.");
 
@@ -107,7 +243,18 @@ void InsertAtmid( )
 
 
     }
-    
+    else
+    {
+        char i;
+        for(i=currentNumber; i>(currentNumber/2);i--)
+        {
+            Shift((st+(i-1)), (st+i));
+        }
+
+        Get_Info((st+(currentNumber/2)));
+
+        currentNumber++;
+    }
 
     setCursorPosition(0, 13);
     printf("The student is inserted successfully.");
@@ -138,14 +285,18 @@ void InsertAtlast(  )
         }
 
     }
-    
+    else
+    {
+        Get_Info((st+currentNumber));
+
+        currentNumber++;
+    }
 
     setCursorPosition(0, 13);
     printf("The student is inserted successfully.");
 
     getch();
 }
-
 
 void welcome()
 {
@@ -183,23 +334,12 @@ void welcome()
     getch();
 }
 
-short choice()
+void choice()
 {
     system("cls");
 
     pointer = 1;
     pressedKey = 0;
-
-    //setCursorPosition(32, 8);
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
-    //setCursorPosition(32, 12);
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-    //printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-
 
     while(1)
     {
@@ -243,15 +383,18 @@ short choice()
             }
             else if(pressedKey == VK_RETURN)
             {
-                return pointer;
+                Choice = pointer;
+                break;
             }
         }
+        if(pressedKey == VK_RETURN)
+            break;
 
     }
 
 }
 
-void Options(short choice)
+void Options( )
 {
     system("cls");
 
@@ -260,7 +403,7 @@ void Options(short choice)
 
     while(1)
     {
-        //fflush(stdin);
+
         system("cls");
 
         setCursorPosition(32, 8);
@@ -307,58 +450,52 @@ void Options(short choice)
                 switch(pointer)
                 {
                 case 1:
-                    printf("\nInsert At beginning.");
-                    Sleep(1000);
+                    InsertAtbeg();
                     break;
 
                 case 2:
-                    printf("\nInsert At middle.");
-                    Sleep(1000);
+                    InsertAtmid();
                     break;
 
                 case 3:
-                    printf("\nInsert At last.");
-                    Sleep(1000);
+                    InsertAtlast();
                     break;
 
                 case 4:
-                    printf("\nprint all data base.");
-                    Sleep(1000);
+                    PrintAll();
                     break;
 
                 case 5:
-                    printf("\nback to menu.");
                     Sleep(1000);
                     break;
 
                 case 6:
-                    printf("\nexit.");
-                    Sleep(1000);
+                    printf("Thank You.");
                     exit(0);
                     break;
                 }
             }
-            if(((pointer == 6) || (pointer == 5))&& (pressedKey == VK_RETURN))
-                break;
+            Sleep(100);
+            break;
         }
-        if (((pointer == 6) || (pointer == 5))&& (pressedKey == VK_RETURN))
+        if (((pointer == 6) || (pointer == 5)) && (pressedKey == VK_RETURN))
             break;
 
     }
 }
 
-
-
 int main()
 {
     hidecursor();
-     st = (Student*)malloc(NumberOfStudent*sizeof(Student));
+
+    st = (Student*)malloc(NumberOfStudent*sizeof(Student));
+
     welcome();
 
     while(1)
     {
-        char c = choice();
-        Options((short)c);
+        choice();
+        Options();
         if(pointer == 6)
             break;
     }
